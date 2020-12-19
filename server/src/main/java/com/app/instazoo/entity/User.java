67fdae893d923c9011099383b0b6623d676ea1aa
AlexanderGolovinov <alexgolovinoff@gmail.com>
2020-12-19 15:40:50 +0200

    CRSE-1 | Create entity models

--- server/src/main/java/com/app/instazoo/entity/User.java
+++ server/src/main/java/com/app/instazoo/entity/User.java
@@ -1,2 +1,51 @@
-package com.app.instazoo.entity;public class User {
+package com.app.instazoo.entity;
+
+import com.app.instazoo.entity.enums.ERole;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String username;
+    private String lastname;
+    @Column(unique = true)
+    private String email;
+    @Column(columnDefinition = "text")
+    private String bio;
+    @ElementCollection(targetClass = ERole.class)
+    @CollectionTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role_name")
+    private Set<ERole> roles = new HashSet<>();
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true)
+    private List<Post> posts = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true)
+    private List<Post> likedPosts = new ArrayList<>();
+
+    @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdDate = LocalDateTime.now();
+    }
+
 }
